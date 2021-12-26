2745cff9dfdb670cf2bb169a8b59449aa573fa49
dwiyono02 <64735824+dwiyono02@users.noreply.github.com>
2021-12-26 17:02:59 +0800

    Delete insert_pengguna.sql

--- insert_pengguna.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO pengguna (id, username, PASSWORD, peran, login_terakhir) VALUES
-		(NULL, 'admin', MD5 ('admin'), 'ADMIN', NULL),
-		(NULL, 'user', MD5 ('user'), 'USER', NULL), 
-		(NULL, 'johndoe', MD5 ('johndoe'), 'USER', NULL),
-		(NULL,'habib', MD5 ('habib'), 'USER', NULL),
-		(NULL,'dwiyono', MD5 ('dwiyono'), 'USER', NULL),
-		(NULL,'gibran', MD5 ('gibran'), 'USER', NULL);
